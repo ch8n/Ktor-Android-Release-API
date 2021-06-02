71c10387734bc2ddc1516b68bf2c4a5932195ae0
ch8n <chetan.garg36@gmail.com>
2021-06-02 23:33:30 +0530

    :heavy_plus_sign: added new route for testing path params

--- src/main/kotlin/com/dev/ch8n/server/routes/GreetingRoutes.kt
+++ src/main/kotlin/com/dev/ch8n/server/routes/GreetingRoutes.kt
@@ -6,12 +6,43 @@ import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
-const val GET_PARAM_NAME = "name"
+private const val GET_PARAM_NAME = "name"
+private const val GET_PARAM_TIMES = "times"
 
 fun Route.greetingRoutes(greetingRepository: GreetingRepository) {
     route("/hello") {
         greet(greetingRepository)
         greetWithNameParam(greetingRepository)
+        greetWithNameParamTimes(greetingRepository)
+    }
+}
+
+private inline fun Route.greetWithNameParamTimes(greetingRepository: GreetingRepository) {
+    get("{$GET_PARAM_NAME}/{$GET_PARAM_TIMES}") {
+        val name = call.parameters.get(GET_PARAM_NAME)
+        val times = call.parameters.get(GET_PARAM_TIMES)?.toIntOrNull()
+
+        if (name.isNullOrEmpty()) {
+            call.respondText(status = HttpStatusCode.BadRequest) {
+                "Tell me your name Stranger!"
+            }
+            return@get
+        }
+
+        if (times == null) {
+            call.respondText(status = HttpStatusCode.BadRequest) {
+                "Tell me how many times $name!"
+            }
+            return@get
+        }
+
+        call.respondText(status = HttpStatusCode.OK) {
+            val result = StringBuilder()
+            repeat(times) {
+                result.append(greetingRepository.getGreeting(name))
+            }
+            result.toString()
+        }
     }
 }
 
